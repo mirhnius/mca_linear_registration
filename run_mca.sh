cd333ecfb60e2d140905e53364456b7f80fbc001
Niusha <niushamirhakimi@gmail.com>
2024-02-13 15:15:46 -0500

    run the command using boutiques

---
+++ run_mca.sh
@@ -0,0 +1,2 @@
+bosh exec launch --imagepath ./glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg ./flirt-fuzzy.json  ./outputs/invocations/anat-12dofs/mca/1/sub-109910_ses-BL_invocation.json
+bosh exec launch --imagepath fsl_fuzzy_feb_12 ./flirt-fuzzy.json  ./outputs/invocations/anat-12dofs/mca/1/sub-109910_ses-BL_invocation.json
