24305756e3bc83ce5c58e7fc986533f6d41856bc
Niusha Sadat Mirhakimi <niusham@narval3.narval.calcul.quebec>
2024-02-15 18:57:45 -0500

    fixing duplication

--- subject_selector.py
+++ subject_selector.py
@@ -8,6 +8,9 @@ import pandas as pd
 PD = "Parkinson's Disease"
 HC = "Healthy Control"
 COHORT = "COHORT_DEFINITION"
+BASELINE = "BL"
+VISIT = "visit"
+ID = "bids_id"
 
 def join_path(*args):
     return str(pathlib.Path(*args).resolve())
@@ -19,24 +22,22 @@ parser.add_argument('--n', type=int, default=50, help='Number of subjects to sel
 
 args = parser.parse_args()
 
-df = pd.read_csv(args.list_path, sep='\t')
-df_healthy = df[df[COHORT] == HC]
-df_parkinsons = df[df[COHORT] == PD]
+df = pd.read_csv(args.list_path)
+df_healthy = df[(df[COHORT] == HC) & (df[VISIT] == BASELINE)]
+df_parkinsons = df[(df[COHORT] == PD) & (df[VISIT] == BASELINE)]
 
 HC_index_selected = np.random.choice(df_healthy.index, args.n, replace=False)
 PD_index_selected = np.random.choice(df_parkinsons.index, args.n, replace=False)
 
-HC_selected_ID = df_healthy.loc[HC_index_selected, COHORT]
-PD_selected_ID = df_parkinsons.loc[PD_index_selected, COHORT]
+HC_selected_ID = df_healthy.loc[HC_index_selected, ID]
+PD_selected_ID = df_parkinsons.loc[PD_index_selected, ID]
 
-HC_selected_paths = HC_selected_ID.applymap(lambda x: join_path(args.input_dir, x))
-PD_selected_paths = PD_selected_ID.applymap(lambda x: join_path(args.input_dir, x))
+HC_selected_paths = HC_selected_ID.apply(lambda x: join_path(args.input_dir, x))
+PD_selected_paths = PD_selected_ID.apply(lambda x: join_path(args.input_dir, x))
 
 HC_selected_paths.to_csv('HC_selected_paths.txt', index=False, header=False)
 PD_selected_paths.to_csv('PD_selected_paths.txt', index=False, header=False)
 
-
-```
 
 
 
