0a80527974f6deac010327e7df5e6a5c21abb432
niusha <niusham@narval3.narval.calcul.quebec>
2024-02-21 11:57:57 -0500

    adding get_matrices function

--- transformation.py
+++ transformation.py
@@ -1,9 +1,14 @@
+import numpy as np
 from pathlib import Path
+
 IEEE = 'ieee'
 MCA = 'mca'
 PATTERN = "*.mat"
 
 def get_matrices_paths(parent_dir, pattern=PATTERN, n_mca=10):
+
+    if isinstance(parent_dir, str):
+        parent_dir = Path(parent_dir)
     ieee_dir = parent_dir / IEEE
     ieee_paths = list(ieee_dir.glob(pattern))
     subjects = [l.name.removesuffix('.mat') for l in ieee_paths]
@@ -14,7 +19,7 @@ def get_matrices_paths(parent_dir, pattern=PATTERN, n_mca=10):
         paths[sub][MCA] = []
  
     mca_dir = parent_dir / MCA
-    for i in range(n_mca):
+    for i in range(1,n_mca+1):
         this_itr_dir = mca_dir / str(i)
         mca_paths = list(this_itr_dir.glob(pattern))
         for sub_path in mca_paths:
@@ -24,9 +29,34 @@ def get_matrices_paths(parent_dir, pattern=PATTERN, n_mca=10):
 
     return paths
 
+def get_matrices(paths):
 
+    matrices = {}
+    for sub in paths.keys():
+    
+        mat_list = []
+        for p in paths[sub][MCA]:
+            mat_list.append(np.loadtxt(p))
+        arr = np.array(mat_list)
+        matrices[sub] = {MCA:arr}
+        
+        p_ieee = paths[sub][IEEE]
+        matrices[sub][IEEE] = np.loadtxt(p_ieee)
 
+    return matrices
 
 if __name__ == '__main__':
     dir = Path('./outputs/anat-12dofs')
-    get_matrices_paths(dir)
+    paths = get_matrices_paths(dir)
+    # n_mca = 10
+    n_subjects = len(paths)
+
+   
+
+
+        
+
+
+
+
+    
