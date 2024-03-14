62a36a9cec00fc6848a72f99d858032fbf2a7a83
niusha <niusham@narval3.narval.calcul.quebec>
2024-03-14 11:40:32 -0400

    updating containers

--- run_ieee_narval.sh
+++ run_ieee_narval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=flirt
-#SBATCH --output=flirt.out
-#SBATCH --error=flirt.err
+#SBATCH --job-name=flirt_PD
+#SBATCH --output=flirt_ieee_PD.out
+#SBATCH --error=flirt_ieee_PD.err
 #SBATCH --time=2:30:0
-#SBATCH --ntasks=3
+#SBATCH --ntasks=50
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=rrg-jbpoline
 
@@ -15,4 +15,4 @@ source /home/niusham/py11/bin/activate
 # parallel "time bosh exec launch --imagepath ./fuzzy_fsl_6.0.4_latest.sif ./flirt-fuzzy.json ./{1}" ::: ./invocations_PD/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
 # parallel "time bosh exec launch --imagepath container_images/glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg descriptors/flirt-fuzzy.json ./{1}" ::: invocations/mca/${SLURM_ARRAY_TASK_ID}/*
 # parallel "time bosh exec launch --imagepath /home/niusham/projects/rrg-glatard/brainhack-2023-linear-registration/container_images/fuzzy_fsl_6.0.4_latest.sif ./flirt-fuzzy.json ./{1}" ::: ./invocations/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
- parallel "time bosh exec launch --imagepath fuzzy_fsl_6.0.4_latest.sif ./flirt.json ./{1}" ::: ./invocations/anat-12dofs/ieee/*
+parallel "time bosh exec launch --imagepath /home/niusham/projects/rrg-glatard/brainhack-2023-linear-registration/container_images/vnmd_fsl_6.0.4-2021-04-22-ac3439c3920c.simg ./flirt.json ./{1}" ::: ./invocations_PD/anat-12dofs/ieee/*
--- run_mca_narval.sh
+++ run_mca_narval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=mca_flirt_PD
-#SBATCH --output=mca_flirt.out
-#SBATCH --error=mca_flirt.err
+#SBATCH --job-name=mca_flirt_HC
+#SBATCH --output=mca_flirt_HC.out
+#SBATCH --error=mca_flirt_HC.err
 #SBATCH --time=2:30:0
 #SBATCH --ntasks=50
 #SBATCH --mem-per-cpu=2G
@@ -14,8 +14,9 @@ source /home/niusham/py11/bin/activate
 #MCA
 # parallel "time bosh exec launch --imagepath ./fuzzy_fsl_6.0.4_latest.sif ./flirt-fuzzy.json ./{1}" ::: ./invocations_PD/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
 # parallel "time bosh exec launch --imagepath container_images/glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg descriptors/flirt-fuzzy.json ./{1}" ::: invocations/mca/${SLURM_ARRAY_TASK_ID}/*
-# parallel "time bosh exec launch --imagepath /home/niusham/projects/rrg-glatard/brainhack-2023-linear-registration/container_images/glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg ./flirt-fuzzy.json ./{1}" ::: ./invocations/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
- parallel "time bosh exec launch --imagepath fuzzy_fsl_6.0.4_latest.sif ./flirt-fuzzy.json ./{1}" ::: ./invocations/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
+parallel "time bosh exec launch --imagepath /home/niusham/projects/rrg-glatard/brainhack-2023-linear-registration/container_images/glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg ./flirt-fuzzy.json ./{1}" ::: ./invocations/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
+# parallel "time bosh exec launch --imagepath fuzzy_fsl_6.0.4_latest.sif ./flirt-fuzzy.json ./{1}" ::: ./invocations_HC/anat-12dofs/mca/${SLURM_ARRAY_TASK_ID}/*
+
 #yohanchatelain-ants-v2.5.0-fuzzy.simg
 #antsx-ants-v2.5.0.simg  
 # glatard_fsl_6.0.4_fuzzy-2023-12-08-a22e376466e7.simg
