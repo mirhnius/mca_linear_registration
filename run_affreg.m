5639fcd86e20e7fba42f476715ce2aa75a359a84
Niusha <niushamirhakimi@gmail.com>
2024-04-29 23:41:46 -0400

    returning transformation matrix and re-sliced registered image

--- run_affreg.m
+++ run_affreg.m
@@ -1,6 +1,7 @@
-function run_affreg(source_img, template_img, output_path)
+function run_affreg(source_img, template_img, output_img_path, output_matrix_path)
+
     % Add SPM to the MATLAB path
-    addpath('/opt/spm12');
+    addpath('/opt/spm12')
 
     % Load the images
     VF = spm_vol(source_img); % Source image volume
@@ -8,18 +9,31 @@ function run_affreg(source_img, template_img, output_path)
 
     % Setup the flags for affine registration
     #flags = struct('regtype', 'affine', 'sep', 4);
+    #should I smooth the images?
 
     % Perform affine registration
     [M, ~] = spm_affreg(VG, VF);
+    #, flags)
 
     % Apply the transformation matrix to the source image
-    VF.mat = M*VF.mat;
-    spm_reslice([VG,VF],struct('which',1));
-    transformed_VF = spm_create_vol(VF);
-    transformed_VF.fname = output_path;
+    VF.mat = M * VF.mat;
 
-    % Write the transformed image to disk
-    spm_write_vol(transformed_VF, spm_read_vols(VF));
+    % Prepare to reslice, but manage outputs manually
+    reslice_flags = struct('which', 1, 'mean', false, 'interp', 1, 'prefix', '');
+    spm_reslice([VG, VF], reslice_flags);
 
-    disp(['Transformed image saved to: ', output_path]);
+    % Load the resliced image (it should be in memory now)
+    resliced_data = spm_read_vols(VF);
+
+    % Update VF structure to save the image with the desired output filename
+    VF.fname = output_img_path;
+
+    % Write the resliced and transformed image data to disk
+    spm_write_vol(VF, resliced_data);
+
+    % Write the transformation matrix to disk
+    save(output_matrix_path, 'M');
+
+    disp(['Transformed and resliced image saved to: ', output_img_path]);
+    disp(['Transformation matrix saved to: ', output_matrix_path]);
 end
