d64a16fe9778638b54dbb29a6d29c336f77cbb79
niusha <niusham@narval2.narval.calcul.quebec>
2024-06-17 17:13:32 -0400

    adding spm config and fixing the function bug

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from lnrgst_mca.load_utils import get_paths
+from lnrgst_mca.load_utils import get_paths, get_matrices
 
 
 configurations = {
@@ -27,6 +27,18 @@ configurations = {
             "pattern_HC": "_ses-BL0GenericAffine",
         }
     },
+    "SPM": {
+        "mni152": {
+            "failed_subjects_HC": [],
+            "failed_subjects_PD": [],
+            "PD_list": Path("./PD_selected_subjects.txt"),
+            "HC_list": Path("./HC_selected_subjects.txt"),
+            "path_PD": Path("./pipline/pd/outputs/spm/anat-12dofs"),
+            "path_HC": Path("./pipline/hc/outputs/spm/anat-12dofs"),
+            "pattern_PD": "_ses-BL",
+            "pattern_HC": "_ses-BL",
+        }
+    },
 }
 
 
@@ -34,14 +46,11 @@ def get_configurations(software, template):
 
     config = configurations[software][template]
 
-    path_PD = config["path_PD"]
-    path_HC = config["path_HC"]
+    paths_PD = get_paths(config["path_PD"], config["PD_list"], pattern=config["pattern_PD"])
+    paths_HC = get_paths(config["path_HC"], config["HC_list"], pattern=config["pattern_HC"])
 
-    list_PD = config["PD_list"]
-    list_HC = config["HC_list"]
-
-    mat_dic_PD, error_PD = get_paths(path_PD, list_PD, pattern=config["pattern_PD"])
-    mat_dic_HC, error_HC = get_paths(path_HC, list_HC, pattern=config["pattern_HC"])
+    mat_dic_PD, error_PD = get_matrices(paths_PD)
+    mat_dic_HC, error_HC = get_matrices(paths_HC)
 
     return config["failed_subjects_HC"], config["failed_subjects_PD"], mat_dic_PD, error_PD, mat_dic_HC, error_HC
 
