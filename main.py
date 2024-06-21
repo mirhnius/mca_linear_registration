de82acf2289ff24e74e7961c765b0e760ae903f7
niusha <niusham@narval2.narval.calcul.quebec>
2024-06-21 18:51:05 -0400

    Adding a parser

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import load_utils
-from plot_utils import plotter
-import metrics_utils
+from lnrgst_mca.plot_utils import plotter
+from lnrgst_mca import metrics_utils
+from config import get_configurations
 from copy import deepcopy
 import numpy as np
+import argparse
 
 # import seaborn as sns
 from pathlib import Path
@@ -76,38 +77,31 @@ def concatenate_mca_matrices(mat_dic):
 
 if __name__ == "__main__":
 
-    # software = "FSL"
-    # failed_subjects_HC = ["sub-116230", "sub-4079", "sub-3620"]
-    # # "sub-3369"
-    # failed_subjects_PD = ["sub-3709", "sub-3700", "sub-3403"]
-    # path = Path().cwd() / "outputs_plots" / "diagrams" / software
+    parser = argparse.ArgumentParser()
 
-    # path_PD = Path("./pipline/pd/outputs/anat-12dofs")
-    # path_HC = Path("./pipline/hc/outputs/anat-12dofs")
+    parser.add_argument("-t", "--template", type=str, help="template name")
+    parser.add_argument("-s", "--software", type=str, help="software name")
+    parser.add_argument("-d", "--diagram_path", type=str, help="path to save output plots")
 
-    # paths_PD = load_utils.get_paths(path_PD, Path("./PD_selected_subjects.txt"), pattern="_ses-BL")
-    # paths_HC = load_utils.get_paths(path_HC, Path("./HC_selected_subjects.txt"), pattern="_ses-BL")
+    args = parser.parse_args()
+    template = args.template
+    software = args.software
+    diagram_path = Path(args.diagram_path)
 
-    software = "ANTS"
-    failed_subjects_HC = ["sub-116230", "sub-3620"]
-    failed_subjects_PD = []
-    path = Path().cwd() / "outputs_plots" / "diagrams" / software
+    # add something to check validity of s and t
+    # loading subject matrix dictonaries and list of failed subjects
+    failed_subjects_HC, failed_subjects_PD, mat_dic_PD, error_PD, mat_dic_HC, error_HC = get_configurations(software, template)
 
-    path_PD = Path("./pipline/pd/outputs/ants/anat-12dofs")
-    path_HC = Path("./pipline/hc/outputs/ants/anat-12dofs")
-
-    paths_PD = load_utils.get_paths(path_PD, Path("./PD_selected_subjects.txt"), pattern="_ses-BL0GenericAffine")
-    paths_HC = load_utils.get_paths(path_HC, Path("./HC_selected_subjects.txt"), pattern="_ses-BL0GenericAffine")
-
-    mat_dic_PD, error_PD = load_utils.get_matrices(paths_PD)
-    mat_dic_HC, error_HC = load_utils.get_matrices(paths_HC)
-
+    # cheking if there is problem in loading mca matrices
     if error_PD or error_HC:
         print(error_PD, error_HC)
         raise Exception("There is an issue with mat files")
 
+    # deviding dictionaries to failed and fine subjects (based on IEEE QC)
     mat_dic_fine_PD, mat_dic_failed_PD = copy_and_remove_keys(mat_dic_PD, failed_subjects_PD)
     mat_dic_fine_HC, mat_dic_failed_HC = copy_and_remove_keys(mat_dic_HC, failed_subjects_HC)
+
+    # decomposing the matrices to 12 parameters
 
     scales_ieee_PD, translation_ieee_PD, angles_ieee_PD, shears_ieee_PD = transformation_dictionary_to_arrays(mat_dic_PD, n_mca=1, mode="ieee")
     scales_ieee_HC, translation_ieee_HC, angles_ieee_HC, shears_ieee_HC = transformation_dictionary_to_arrays(mat_dic_HC, n_mca=1, mode="ieee")
@@ -139,12 +133,7 @@ if __name__ == "__main__":
         mat_dic_failed_HC
     )
 
-    n_fine_HC = len(mat_dic_fine_HC)
-    n_fine_PD = len(mat_dic_fine_PD)
-
-    n_failed_HC = len(mat_dic_fine_HC)
-    n_failed_PD = len(mat_dic_fine_PD)
-
+    # change radians vectors to degrees
     angles_mca_PD = np.degrees(angles_mca_PD)
     angles_mca_HC = np.degrees(angles_mca_HC)
 
@@ -163,64 +152,60 @@ if __name__ == "__main__":
     angles_ieee_failed_PD = np.degrees(angles_ieee_failed_PD)
     angles_ieee_failed_HC = np.degrees(angles_ieee_failed_HC)
 
+    # plotting mean and standard deviation for 12 parameters
+
     basic_info_plotter(
         translation_mca_fine_PD,
         translation_mca_fine_HC,
         software=software,
         variable="Translations",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         ylable="(mm)",
         y_lim_mean=[(-14, 12), (-60, 25), (-42, 61)],
         y_lim_sd=[(0, 0.1), (0, 0.2), (0, 0.2)],
     )
-    # y_lim_mean=[(-25, 25), (-50, 20), (-45, 45)],
-    # y_lim_sd=[(0, 0.08), (0, 0.2), (0, 0.06)],
-    # y_lim_sd=[(0, 0.08), (0, 0.2), (0, 0.06)]
 
     basic_info_plotter(
         angles_mca_fine_PD,
         angles_mca_fine_HC,
         software=software,
         variable="Angles",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         ylable="(degree)",
         y_lim_mean=[(-40, 5), (-10.5, 8), (-11, 11)],
         y_lim_sd=[(0, 0.5), (0, 0.25), (0, 0.26)],
     )
-    # fsl y_lim_mean=[(-35, 5), (-9, 7), (-7, 5)],
-    # y_lim_sd=[(0, 4), (0, 0.2), (0, 0.2)],
 
     basic_info_plotter(
         scales_mca_fine_PD,
         scales_mca_fine_HC,
         software=software,
         variable="Scales",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         y_lim_mean=[(0.825, 1.27), (0.8, 1.32), (0.7, 1.35)],
         y_lim_sd=[(0, 0.002), (0, 0.0025), (0, 0.005)],
     )
-    # y_lim_sd=[(0, 0.01), (0, 0.1), (0, 1)],
 
     basic_info_plotter(
         shears_mca_fine_PD,
         shears_mca_fine_HC,
         software=software,
         variable="Shears",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         y_lim_mean=[(-0.065, 0.065), (-0.045, 0.045), (-0.22, 0.105)],
         y_lim_sd=[(0, 0.003), (0, 0.004), (0, 0.008)],
     )
-    # y_lim_sd=[(0, 0.05), (0, 0.1), (0, 1)],
+
     basic_info_plotter(
         translation_mca_failed_PD,
         translation_mca_failed_HC,
         software=software,
         variable="Translations failed",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         ylable="(mm)",
     )
@@ -229,16 +214,18 @@ if __name__ == "__main__":
         angles_mca_failed_HC,
         software=software,
         variable="Angles failed",
-        path=path,
+        path=diagram_path,
         axis_labels=["x", "y", "z"],
         ylable="(degree)",
     )
     basic_info_plotter(
-        scales_mca_failed_PD, scales_mca_failed_HC, software=software, variable="Scales failed", path=path, axis_labels=["x", "y", "z"]
+        scales_mca_failed_PD, scales_mca_failed_HC, software=software, variable="Scales failed", path=diagram_path, axis_labels=["x", "y", "z"]
     )
     basic_info_plotter(
-        shears_mca_failed_PD, shears_mca_failed_HC, software=software, variable="Shears failed", path=path, axis_labels=["x", "y", "z"]
+        shears_mca_failed_PD, shears_mca_failed_HC, software=software, variable="Shears failed", path=diagram_path, axis_labels=["x", "y", "z"]
     )
+
+    # calculating framewise displacment
 
     result_fine_PD = metrics_utils.FD_all_subjects(translation_mca_fine_PD, angles_mca_fine_PD)
     result_fine_HC = metrics_utils.FD_all_subjects(translation_mca_fine_HC, angles_mca_fine_HC)
@@ -258,46 +245,52 @@ if __name__ == "__main__":
     result_all_ieee_PD = metrics_utils.FD_all_subjects(translation_ieee_PD[:, np.newaxis, :], angles_ieee_PD[:, np.newaxis, :])
     result_all_ieee_HC = metrics_utils.FD_all_subjects(translation_ieee_HC[:, np.newaxis, :], angles_ieee_HC[:, np.newaxis, :])
 
+    # plotting framewise displacment
     basic_info_plotter(
         result_fine_PD,
         result_fine_HC,
         software=software,
         variable="Framewise Displacement",
-        path=path,
+        path=diagram_path,
         figure_size=(5, 4),
         ylable="(mm)",
         y_lim_mean=[(10, 110)],
         y_lim_sd=[(0, 0.3)],
     )
-    #    y_lim_sd=[(0,4), (0,0.2), (0,0.2)])
-    #  fsl y_lim_sd=[(0,.3)]
-    #  y_lim_sd=[(0, 2)],
-    # y_lim_mean=[(15,110)]
+
     basic_info_plotter(
-        result_failed_PD, result_failed_HC, software=software, variable="Framewise Displacement failed", path=path, figure_size=(5, 4), ylable="(mm)"
+        result_failed_PD,
+        result_failed_HC,
+        software=software,
+        variable="Framewise Displacement failed",
+        path=diagram_path,
+        figure_size=(5, 4),
+        ylable="(mm)",
     )
     basic_info_plotter(
-        result_all_PD, result_all_HC, software=software, variable="Framewise Displacement All", path=path, figure_size=(5, 4), ylable="(mm)"
+        result_all_PD, result_all_HC, software=software, variable="Framewise Displacement All", path=diagram_path, figure_size=(5, 4), ylable="(mm)"
     )
+    # saving FD
+    path = diagram_path.parent
+    np.savetxt(path / f"{software}_FD_PD_all.txt", result_all_PD)
+    np.savetxt(path / f"{software}_FD_HC_all.txt", result_all_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_PD_all.txt", result_all_PD)
-    np.savetxt(path.parent / f"{software}_FD_HC_all.txt", result_all_HC)
+    np.savetxt(path / f"{software}_FD_PD_failed.txt", result_failed_PD)
+    np.savetxt(path / f"{software}_FD_HC_failed.txt", result_failed_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_PD_failed.txt", result_failed_PD)
-    np.savetxt(path.parent / f"{software}_FD_HC_failed.txt", result_failed_HC)
+    np.savetxt(path / f"{software}_FD_PD_fine.txt", result_fine_PD)
+    np.savetxt(path / f"{software}_FD_HC_fine.txt", result_fine_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_PD_fine.txt", result_fine_PD)
-    np.savetxt(path.parent / f"{software}_FD_HC_fine.txt", result_fine_HC)
+    np.savetxt(path / f"{software}_FD_ieee_PD_all.txt", result_all_ieee_PD)
+    np.savetxt(path / f"{software}_FD_ieee_HC_all.txt", result_all_ieee_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_ieee_PD_all.txt", result_all_ieee_PD)
-    np.savetxt(path.parent / f"{software}_FD_ieee_HC_all.txt", result_all_ieee_HC)
+    np.savetxt(path / f"{software}_FD_ieee_PD_failed.txt", result_failed_ieee_PD)
+    np.savetxt(path / f"{software}_FD_ieee_HC_failed.txt", result_failed_ieee_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_ieee_PD_failed.txt", result_failed_ieee_PD)
-    np.savetxt(path.parent / f"{software}_FD_ieee_HC_failed.txt", result_failed_ieee_HC)
+    np.savetxt(path / f"{software}_FD_ieee_PD_fine.txt", result_fine_ieee_PD)
+    np.savetxt(path / f"{software}_FD_ieee_HC_fine.txt", result_fine_ieee_HC)
 
-    np.savetxt(path.parent / f"{software}_FD_ieee_PD_fine.txt", result_fine_ieee_PD)
-    np.savetxt(path.parent / f"{software}_FD_ieee_HC_fine.txt", result_fine_ieee_HC)
-
+    # calculating the mean absolute difference between MCA framewise displacements and the IEEE framewise displacement
     mad_fine_PD = metrics_utils.mean_absolute_difference(result_fine_PD, result_fine_ieee_PD)
     mad_fine_HC = metrics_utils.mean_absolute_difference(result_fine_HC, result_fine_ieee_HC)
 
@@ -307,67 +300,114 @@ if __name__ == "__main__":
     mad_all_PD = metrics_utils.mean_absolute_difference(result_all_PD, result_all_ieee_PD)
     mad_all_HC = metrics_utils.mean_absolute_difference(result_all_HC, result_all_ieee_HC)
 
-    np.savetxt(path.parent / f"{software}_mad_fine_PD.txt", mad_fine_PD)
-    np.savetxt(path.parent / f"{software}_mad_fine_HC.txt", mad_fine_HC)
+    # saving MAD
+    np.savetxt(path / f"{software}_mad_fine_PD.txt", mad_fine_PD)
+    np.savetxt(path / f"{software}_mad_fine_HC.txt", mad_fine_HC)
 
-    np.savetxt(path.parent / f"{software}_mad_failed_PD.txt", mad_failed_PD)
-    np.savetxt(path.parent / f"{software}_mad_failed_HC.txt", mad_failed_HC)
+    np.savetxt(path / f"{software}_mad_failed_PD.txt", mad_failed_PD)
+    np.savetxt(path / f"{software}_mad_failed_HC.txt", mad_failed_HC)
 
-    np.savetxt(path.parent / f"{software}_mad_all_PD.txt", mad_all_PD)
-    np.savetxt(path.parent / f"{software}_mad_all_HC.txt", mad_all_HC)
+    np.savetxt(path / f"{software}_mad_all_PD.txt", mad_all_PD)
+    np.savetxt(path / f"{software}_mad_all_HC.txt", mad_all_HC)
 
-    new_result_fine_PD = metrics_utils.FD_all_subjects(translation_mca_fine_PD, angles_mca_fine_PD, translation_ieee_fine_PD, angles_ieee_fine_PD)
-    new_result_fine_HC = metrics_utils.FD_all_subjects(translation_mca_fine_HC, angles_mca_fine_HC, translation_ieee_fine_HC, angles_ieee_fine_HC)
+    # new_result_fine_PD = metrics_utils.FD_all_subjects(translation_mca_fine_PD, angles_mca_fine_PD, translation_ieee_fine_PD, angles_ieee_fine_PD)
+    # new_result_fine_HC = metrics_utils.FD_all_subjects(translation_mca_fine_HC, angles_mca_fine_HC, translation_ieee_fine_HC, angles_ieee_fine_HC)
 
-    new_result_failed_PD = metrics_utils.FD_all_subjects(
-        translation_mca_failed_PD, angles_mca_failed_PD, translation_ieee_failed_PD, angles_ieee_failed_PD
-    )
-    new_result_failed_HC = metrics_utils.FD_all_subjects(
-        translation_mca_failed_HC, angles_mca_failed_HC, translation_ieee_failed_HC, angles_ieee_failed_HC
-    )
+    # new_result_failed_PD = metrics_utils.FD_all_subjects(
+    #     translation_mca_failed_PD, angles_mca_failed_PD, translation_ieee_failed_PD, angles_ieee_failed_PD
+    # )
+    # new_result_failed_HC = metrics_utils.FD_all_subjects(
+    #     translation_mca_failed_HC, angles_mca_failed_HC, translation_ieee_failed_HC, angles_ieee_failed_HC
+    # )
 
-    new_result_all_PD = metrics_utils.FD_all_subjects(translation_mca_PD, angles_mca_PD, translation_ieee_PD, angles_ieee_PD)
-    new_result_all_HC = metrics_utils.FD_all_subjects(translation_mca_HC, angles_mca_HC, translation_ieee_HC, angles_ieee_HC)
+    # new_result_all_PD = metrics_utils.FD_all_subjects(translation_mca_PD, angles_mca_PD, translation_ieee_PD, angles_ieee_PD)
+    # new_result_all_HC = metrics_utils.FD_all_subjects(translation_mca_HC, angles_mca_HC, translation_ieee_HC, angles_ieee_HC)
 
-    basic_info_plotter(
-        new_result_fine_PD,
-        new_result_fine_HC,
-        software=software,
-        variable="Framewise Displacement (IEEE Reference)",
-        path=path,
-        figure_size=(5, 4),
-        ylable="(mm)",
-    )
-    #         y_lim_sd=[(0, 0.5)],
-    # y_lim_mean=[(0, 1)],
-    basic_info_plotter(
-        new_result_failed_PD,
-        new_result_failed_HC,
-        software=software,
-        variable="Framewise Displacement failed (IEEE Reference)",
-        path=path,
-        figure_size=(5, 4),
-        ylable="(mm)",
-    )
-    basic_info_plotter(
-        new_result_all_PD,
-        new_result_all_HC,
-        software=software,
-        variable="Framewise Displacement All(IEEE Reference)",
-        path=path,
-        figure_size=(5, 4),
-        ylable="(mm)",
-    )
+    # basic_info_plotter(
+    #     new_result_fine_PD,
+    #     new_result_fine_HC,
+    #     software=software,
+    #     variable="Framewise Displacement (IEEE Reference)",
+    #     path=path,
+    #     figure_size=(5, 4),
+    #     ylable="(mm)",
+    # )
 
-    np.savetxt(path.parent / f"{software}_new_FD_PD_all.txt", new_result_all_PD)
-    np.savetxt(path.parent / f"{software}_new_FD_HC_all.txt", new_result_all_HC)
+    # basic_info_plotter(
+    #     new_result_failed_PD,
+    #     new_result_failed_HC,
+    #     software=software,
+    #     variable="Framewise Displacement failed (IEEE Reference)",
+    #     path=path,
+    #     figure_size=(5, 4),
+    #     ylable="(mm)",
+    # )
+    # basic_info_plotter(
+    #     new_result_all_PD,
+    #     new_result_all_HC,
+    #     software=software,
+    #     variable="Framewise Displacement All(IEEE Reference)",
+    #     path=path,
+    #     figure_size=(5, 4),
+    #     ylable="(mm)",
+    # )
 
-    np.savetxt(path.parent / f"{software}_new_FD_PD_failed.txt", new_result_failed_PD)
-    np.savetxt(path.parent / f"{software}_new_FD_HC_failed.txt", new_result_failed_HC)
+    # np.savetxt(path.parent / f"{software}_new_FD_PD_all.txt", new_result_all_PD)
+    # np.savetxt(path.parent / f"{software}_new_FD_HC_all.txt", new_result_all_HC)
 
-    np.savetxt(path.parent / f"{software}_new_FD_PD_fine.txt", new_result_fine_PD)
-    np.savetxt(path.parent / f"{software}_new_FD_HC_fine.txt", new_result_fine_HC)
+    # np.savetxt(path.parent / f"{software}_new_FD_PD_failed.txt", new_result_failed_PD)
+    # np.savetxt(path.parent / f"{software}_new_FD_HC_failed.txt", new_result_failed_HC)
 
+    # np.savetxt(path.parent / f"{software}_new_FD_PD_fine.txt", new_result_fine_PD)
+    # np.savetxt(path.parent / f"{software}_new_FD_HC_fine.txt", new_result_fine_HC)
+
+    # software = "FSL"
+    # failed_subjects_HC = ["sub-116230", "sub-4079", "sub-3620"]
+    # # "sub-3369"
+    # failed_subjects_PD = ["sub-3709", "sub-3700", "sub-3403"]
+    # path = Path().cwd() / "outputs_plots" / "diagrams" / software
+
+    # path_PD = Path("./pipline/pd/outputs/anat-12dofs")
+    # path_HC = Path("./pipline/hc/outputs/anat-12dofs")
+
+    # paths_PD = load_utils.get_paths(path_PD, Path("./PD_selected_subjects.txt"), pattern="_ses-BL")
+    # paths_HC = load_utils.get_paths(path_HC, Path("./HC_selected_subjects.txt"), pattern="_ses-BL")
+
+    # software = "ANTS"
+    # failed_subjects_HC = ["sub-116230", "sub-3620"]
+    # failed_subjects_PD = []
+    # path = Path().cwd() / "outputs_plots" / "diagrams" / software
+
+    # path_PD = Path("./pipline/pd/outputs/ants/anat-12dofs")
+    # path_HC = Path("./pipline/hc/outputs/ants/anat-12dofs")
+
+    # paths_PD = load_utils.get_paths(path_PD, Path("./PD_selected_subjects.txt"), pattern="_ses-BL0GenericAffine")
+    # paths_HC = load_utils.get_paths(path_HC, Path("./HC_selected_subjects.txt"), pattern="_ses-BL0GenericAffine")
+
+    # mat_dic_PD, error_PD = load_utils.get_matrices(paths_PD)
+    # mat_dic_HC, error_HC = load_utils.get_matrices(paths_HC)
+
+    # software = "SPM"
+    # failed_subjects_HC = ["sub-116230", "sub-3620",  "sub-3316",
+    #   "sub-3361",  "sub-3414",  "sub-3570",  "sub-3615",  "sub-3811",  "sub-3853",  "sub-3969",  "sub-4079"]
+    # failed_subjects_PD = ["sub-3403", "sub-3586", "sub-3700",
+    #  "sub-3709", "sub-3823", "sub-3960", "sub-3970", "sub-4121", "sub-40733", "sub-42264", "sub-75562"]
+    # path = Path().cwd() / "outputs_plots" / "diagrams" / software
+
+    # path_PD = Path("./pipline/pd/outputs/spm/anat-12dofs")
+    # path_HC = Path("./pipline/hc/outputs/spm/anat-12dofs")
+
+    # paths_PD = load_utils.get_paths(path_PD, Path("./PD_selected_subjects.txt"), pattern="_ses-BL")
+    # paths_HC = load_utils.get_paths(path_HC, Path("./HC_selected_subjects.txt"), pattern="_ses-BL")
+
+    # mat_dic_PD, error_PD = load_utils.get_matrices(paths_PD)
+    # mat_dic_HC, error_HC = load_utils.get_matrices(paths_HC)
+
+    # if error_PD or error_HC:
+    #     print(error_PD, error_HC)
+    #     raise Exception("There is an issue with mat files")
+
+    # from lnrgst_mca import load_utils
     # largest_indces(np.std(result_all_PD, axis=1), mat_dic_PD, n=10)
     # largest_indces(np.std(result_all_HC, axis=1), mat_dic_HC, n=10)
 
