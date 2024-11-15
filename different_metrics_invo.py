586c59777768f367e9f06814ef47a0b7a80175a9
niusha <niusham@narval2.narval.calcul.quebec>
2024-11-14 23:17:28 -0500

    fixing an invocation bug

--- different_metrics_invo.py
+++ different_metrics_invo.py
@@ -20,25 +20,20 @@ def scan_filed_dict_short(scan_path):
 # p = Path('/home/niusham/projects/rrg-jbpoline/niusham/mca_linear_registration/pipline/hc/outputs/preprocess/sub-3112_ses-BL.nii.gz')
 # scan_filed_dict(p)
 
-map_ = invo.create_subject_map(in_path, pattern=pattern, scanner=scan_filed_dict_short)
-
 
 ref = Path("./tpl-MNI152NLin2009cAsym_res-01_T1w_neck_5.nii.gz")
 
 
 for group in ["hc", "pd"]:
     in_path = Path(f"./pipline/{group}/outputs/preprocess")
-    output_dir = Path(f"./metrics_exp/{group}/output")
-    invocation_dir = Path(f"./metrics_exp/{group}/invocation")
+    output_dir = Path(f"/home/niusham/metrics_exp/{group}/output")
+    invocation_dir = Path(f"/home/niusham/metrics_exp/{group}/invocation")
+    map_ = invo.create_subject_map(in_path, pattern=pattern, scanner=scan_filed_dict_short)
     for method in ["mutualinfo", "normcorr", "normmi", "leastsq"]:
 
         output_path = output_dir / method
         invocation_path = invocation_dir / method
 
-        invo.FLIRT_IEEE_registration(
-            map_, output_path, invocation_path, ref=ref, template_name="2009Asym", cost_function="mutualinfo"
-        ).create_invocations()
+        invo.FLIRT_IEEE_registration(map_, output_path, invocation_path, ref=ref, template_name="2009Asym", cost_function=method).create_invocations()
 
-        invo.FLIRT_MCA_registration(
-            map_, output_path, invocation_path, ref=ref, template_name="2009Asym", cost_function="mutualinfo"
-        ).create_invocations()
+        invo.FLIRT_MCA_registration(map_, output_path, invocation_path, ref=ref, template_name="2009Asym", cost_function=method).create_invocations()
