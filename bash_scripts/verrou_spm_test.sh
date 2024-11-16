8c7f1a0aef758d02f93c7cb2b8e1cc295eea7b15
Niusha <niushamirhakimi@gmail.com>
2024-11-16 17:25:15 -0500

    bash script to test verrou - spm on a subject

---
+++ bash_scripts/verrou_spm_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Verrou SPM test
+
+SOURCE_IMAGE="sub-3000_out.nii"
+TEMPLATE_IMAGE="./tpl-MNI152NLin2009cAsym_res-01_T1w_roi_neck_test.nii"
+OUTPUT_PATH="spm_verrou_output"
+# REGISTERED_IMAGE=""
+# TRANSFORMATION_MATRIX=""
+N_RUNS=10
+
+for i in $(seq 1 $N_RUNS); do
+    OUTPUT_DIR="${OUTPUT_PATH}/run_${i}"
+    mkdir -p "$OUTPUT_DIR"
+
+    docker run --rm -v $(pwd):/code -w /code \
+        verrouspm valgrind --tool=verrou --rounding-mode=random octave --no-gui --eval "run_affreg('${SOURCE_IMAGE}', '${TEMPLATE_IMAGE}', '${OUTPUT_DIR}/registered_image.nii', '${OUTPUT_DIR}/transformation_matrix.txt')"
+done
+wait
+echo "All runs completed"
+
